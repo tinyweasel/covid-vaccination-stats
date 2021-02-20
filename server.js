906929b9e92d5f745cd0479e92619ec4e12769e8
Tanya Branagan <tbranagan@vectra.ai>
2021-02-20 20:23:38 +0000

    Require path

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { getCovidData } = require('./get-vaccination-data')
 
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
